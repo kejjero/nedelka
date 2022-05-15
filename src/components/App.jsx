f38cf59a0b2ed03e64d139204962868d7c5dfa48
Maxim <kejerino@yandex.ru>
2022-05-15 21:05:38 +0800

    feat: created PopupForCards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Main from "./Main";
 import Footer from "./Footer";
 import {useEffect, useState} from "react";
 import axios from "axios";
+import PopupForCards from "./PopupForCards";
 
 const API_KEY = 'https://api.openweathermap.org/data/2.5/onecall?lat=52.3&lon=104.3&units=metric&&lang=ru&appid=64caa5601a72cd6bc9eb103f1d7c6a04'
 
@@ -19,11 +20,14 @@ function App() {
 
     return (
       <>
-          <Header/>
+          <Header
+
+          />
           <Main
               dataWeather={dataWeather}
           />
           <Footer/>
+          <PopupForCards/>
       </>
   )
 }
