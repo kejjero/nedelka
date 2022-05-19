2ee05307616d7b83a17477ad4737fcab47155b41
Maxim <kejerino@yandex.ru>
2022-05-19 12:57:15 +0800

    fix!: fixed keys for cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,9 +6,9 @@ import LoadingCards from "./LoadingCards";
 function Main (props) {
     // убираем 8 карточку из запроса
     const dataWeather = props.dataWeather.slice(0, -1);
-
+    // проверяем, получили ли мы данные для заполнения карточек
     const isLoadingCards = props.dataWeather.length === 0;
-    console.log(isLoadingCards)
+
     return (
         <main className={styles.main}>
             <img className={styles.city} src={city} alt="Город"/>
@@ -17,10 +17,10 @@ function Main (props) {
             <div className={styles.wrapper}>
                 <ul className={styles.cards}>
                     {isLoadingCards ? <LoadingCards/> :
-                        dataWeather.map((data) => {
+                        dataWeather.map((data, index) => {
                         return (
                             <Card
-                                    key={data.dt}
+                                    key={index}
                                     temp={data.temp.day}
                                     date={data.dt}
                                     description={data.weather[0].description}
